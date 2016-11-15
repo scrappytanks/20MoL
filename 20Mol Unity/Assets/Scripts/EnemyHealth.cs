a3f9873e589ccdb01ecc95f09aa087f47dfaaac6
Aaron McIntyre <aaronjames18@hotmail.com>
2016-11-15 21:20:00 +1100

    no message

---
+++ 20Mol Unity/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+
+    public float m_StrartingHealth = 100f;
+
+
+    public float m_CurrentHealth;
+
+    private bool m_Dead;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	private void OnEnable() {
+
+        m_CurrentHealth = m_StrartingHealth;
+        m_Dead = false;
+
+	
+	}
+
+    private void TakeDamage(float amount)
+    {
+        m_CurrentHealth -= amount;
+
+        if (m_CurrentHealth <= 0f && !m_Dead)
+        {
+            OnDeath();
+        }
+ }
+
+    private void OnDeath()
+    {
+        m_Dead = true;
+
+        gameObject.SetActive(false);
+
+    }
+
+
+}
--- 20Mol Unity/Assets/Scripts/PlayerHealth.cs
+++ 20Mol Unity/Assets/Scripts/PlayerHealth.cs
@@ -1,31 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerHealth : MonoBehaviour {
+public class PlayerHealth : MonoBehaviour
+{
 
-    public int curHealth;
-    public int maxHealth = 20;
 
-	// Use this for initialization
-	void Start () {
-        curHealth = maxHealth;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (curHealth > maxHealth)
-        {
-            curHealth = maxHealth;
-        }
-        if (curHealth <= 0)
-        {
-            Die();
-        }
-	}
+    public float m_StrartingHealth = 20;
 
-    void Die()
+
+    public float m_CurrentHealth;
+
+    private bool Dead;
+
+
+
+
+
+    // Use this for initialization
+    void Start()
     {
-        //Don't need this yet
-        //Application.LoadLevel(Application.loadedLevel);
+
     }
+
+    // Update is called once per frame
+    private void OnEnable()
+    {
+
+        m_CurrentHealth = m_StrartingHealth;
+        Dead = false;
+
+
+    }
+
+    private void TakeDamage(float amount)
+    {
+        m_CurrentHealth -= amount;
+
+        if (m_CurrentHealth <= 0 && !Dead)
+        {
+            OnDeath();
+        }
+    }
+
+    public void OnDeath()
+    {
+
+
+        if (Dead == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
 }
