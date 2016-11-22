a2999777f8e57678d5e9085f269ae4150139bd20
Marcus Parry <marcus.parry@ad.aie.edu>
2016-11-22 18:13:37 +1100

    enemy asset

---
+++ 20Mol Unity/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public Transform Target;
+    private GameObject Enemy;
+    private GameObject Player;
+    private float Range;
+    private float Speed = 0.5f;
+    private bool playerisinRange;
+
+    // Use this for initialization
+    void Start() {
+        Enemy = GameObject.FindGameObjectWithTag("Enemy");
+        Player = GameObject.FindGameObjectWithTag("Player");
+        transform.position = new Vector2(-31.42f, -2.78f);
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Range = Vector2.Distance(Enemy.transform.position, Player.transform.position);
+        if (playerisinRange)
+        {
+            //Range = Vector2.Distance(Enemy.transform.position, Player.transform.position);
+            //transform.LookAt(Target);
+            Enemy.transform.position = Vector2.MoveTowards(Enemy.transform.position, Player.transform.position, Speed * Time.deltaTime) ;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.gameObject == Player)
+        {
+            Debug.Log("follow the player");
+            //Range = Vector2.Distance(Enemy.transform.position, Player.transform.position);
+            //if (Range >= 15f)
+            //{
+            //    transform.Translate(Vector2.MoveTowards(Enemy.transform.position, Target.position, 100) * Speed * Time.deltaTime);
+            //}
+            playerisinRange = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == Player)
+        {
+            playerisinRange = false;
+        }
+    }
+}
