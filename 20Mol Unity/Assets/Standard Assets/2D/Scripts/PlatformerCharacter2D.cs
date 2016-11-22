4e6d161e76863c7972129dcd25f3f5378133579b
Marcus Parry <marcus.parry@ad.aie.edu>
2016-11-22 20:47:10 +1100

    sound added to pizzarobot

--- 20Mol Unity/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ 20Mol Unity/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -19,6 +19,7 @@ namespace UnityStandardAssets._2D
         private Animator m_Anim;            // Reference to the player's animator component.
         private Rigidbody2D m_Rigidbody2D;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+        AudioSource JumpSource;
 
         private void Awake()
         {
@@ -27,6 +28,7 @@ namespace UnityStandardAssets._2D
             m_CeilingCheck = transform.Find("CeilingCheck");
             m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
+            JumpSource = GetComponent<AudioSource>();
         }
 
 
@@ -96,6 +98,7 @@ namespace UnityStandardAssets._2D
                 m_Grounded = false;
                 m_Anim.SetBool("Ground", false);
                 m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+                JumpSource.Play();
             }
         }
 
