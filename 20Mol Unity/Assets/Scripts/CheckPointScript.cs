78c69d6c50ffd3db1043a0a89cab18692efded9d
William Gallo <william.gallo@ad.aie.edu>
2016-11-22 17:45:26 +1100

    Changes from last week. New assets and checkpoints added

---
+++ 20Mol Unity/Assets/Scripts/CheckPointScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPointScript : MonoBehaviour {
+
+    public Transform SpawnPoint;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Respawn")
+        {
+            SpawnPoint.position = new Vector3(transform.position.x, transform.position.y, SpawnPoint.position.z);
+        }
+    }
+}
