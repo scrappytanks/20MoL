91f058229fcca78a55a10fc335bcbff162e44e7a
William Gallo <william.gallo@ad.aie.edu>
2016-11-24 18:30:31 +1100

    enemy fixed!! thanks Jared

--- 20Mol Unity/Assets/Scripts/EnemyMovement.cs
+++ 20Mol Unity/Assets/Scripts/EnemyMovement.cs
@@ -4,19 +4,17 @@ using System.Collections;
 public class EnemyMovement : MonoBehaviour {
 
     public Transform Target;
-    private GameObject Enemy;
     private GameObject Player;
     private float Range;
     public float Speed = 0.5f;
     private bool playerisinRange;
 
-    [HideInInspector]
+    
     public bool facingRight = true;
-    [HideInInspector]
+    
 
     // Use this for initialization
     void Start() {
-        Enemy = GameObject.FindGameObjectWithTag("Enemy");
         Player = GameObject.FindGameObjectWithTag("Player");
         
 
@@ -24,27 +22,27 @@ public class EnemyMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        Range = Vector2.Distance(Enemy.transform.position, Player.transform.position);
+        Range = Vector2.Distance(transform.position, Player.transform.position);
         if (playerisinRange)
         {
-            Range = Vector2.Distance(Enemy.transform.position, Player.transform.position);
-            //transform.LookAt(Target);
-            Enemy.transform.position = Vector2.MoveTowards(Enemy.transform.position, Player.transform.position, Speed * Time.deltaTime) ;
-        }
-        float h = Input.GetAxis("Horizontal");
+            float h = Player.transform.position.x - transform.position.x;
 
-        if (h > 0 && !facingRight)
-            Flip();
-        else if (h < 0 && facingRight)
-            Flip();
+            if (h > 0 && !facingRight)
+                Flip();
+            else if (h < 0 && facingRight)
+                Flip();
+
+            //transform.LookAt(Target);
+            transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, Speed * Time.deltaTime) ;
+        }
+        
     }
 
     void Flip()
     {
         facingRight = !facingRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
+        SpriteRenderer SR = GetComponent<SpriteRenderer>();
+        SR.flipX = facingRight;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -52,12 +50,7 @@ public class EnemyMovement : MonoBehaviour {
         
         if (other.gameObject == Player)
         {
-            Debug.Log("follow the player");
-            Range = Vector2.Distance(Enemy.transform.position, Player.transform.position);
-            if (Range >= 15f)
-            {
-                transform.Translate(Vector2.MoveTowards(Enemy.transform.position, Target.position, 100) * Speed * Time.deltaTime);
-            }
+            
             playerisinRange = true;
         }
     }
