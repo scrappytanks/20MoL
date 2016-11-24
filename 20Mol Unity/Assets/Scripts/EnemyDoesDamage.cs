5782ac711c673ddf38fb19484c8c2984c67c36e1
William Gallo <william.gallo@ad.aie.edu>
2016-11-24 19:36:11 +1100

    Damage script for enemy, work in progress

---
+++ 20Mol Unity/Assets/Scripts/EnemyDoesDamage.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDoesDamage : MonoBehaviour {
+
+    public float damage = 5f;
+
+    void OnCollision2DEnter(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+        {
+            this.GetComponent<PlayerHealth>().m_CurrentHealth -= 1;
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
